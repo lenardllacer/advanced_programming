e4643487fed97721e88bfb4ae19e2e95ad134ace
Lenard Llacer <linijin17@gmail.com>
2020-11-08 03:20:57 +0800

    This is index.php with adding a friend and posting a status

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-  echo "Hello world";
+    class User{
 
+      public $name;
+      public $username;
+      public $email;
+
+      function addFriend($name){
+        echo "$name" , " added a friend. <br>";
+      }
+      function postStatus($name){
+        echo "$name" . " posted a status. <br>";
+      }
+    }
+    $example = new User();
+    echo $example->addFriend('Lenard Llacer');
+    echo "<br>";
+    echo $example->postStatus('Lenard Llacer');
  ?>
